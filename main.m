c67d6ad7be92423728c89ac4cbeb566e0b6caa5f
lindol <pces51304@gmail.com>
2015-01-11 18:26:50 +0800

    merge 2 court detection and player detection

--- main.m
+++ main.m
@@ -4,7 +4,6 @@ close all;
 
 addpath('src/');
 frameIdx = [200 204];
-ballDetection('1.avi',200);
-% [ frameNum, court, topLeft, botLeft, topRight, botRight ] = courtDetection('1.avi', frameIdx);
-% [ courtDownHalf ] = playerTrack('1.avi', frameIdx, topLeft, topRight, botLeft, botRight);
+[ frameNum, court, topLeft, botLeft, topRight, botRight ] = courtDetection('1.avi', frameIdx);
+[ courtDownHalf ] = playerTrack('1.avi', frameIdx, topLeft, topRight, botLeft, botRight);
 
