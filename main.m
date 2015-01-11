345efd7ce80e950aa6eeb431bba41815654f3ae4
lindol <pces51304@gmail.com>
2015-01-11 21:02:17 +0800

    do nothing

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear all;
 close all;
 
 addpath('src/');
-frameIdx = [200 204];
+frameIdx = 204;
 [ frameNum, court, topLeft, botLeft, topRight, botRight ] = courtDetection('1.avi', frameIdx);
-[ courtDownHalf ] = playerTrack('1.avi', frameIdx, topLeft, topRight, botLeft, botRight);
+[PlayInUpCol, PlayInUpRow, PlayInDownCol, PlayInDownRow] = playerTrack('1.avi', frameIdx, topLeft, topRight, botLeft, botRight);
 
