72f5f21920b8e844ad7558eb52aaf0cdaa3104b6
Plabo1028 <qewradsf1028@gmail.com>
2015-01-13 01:16:59 +0800

    Debug
    換成  球場上跟下的rows cols

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear all;
 close all;
 
 addpath('src\');
-for i = 204:10:2000
+for i = 1200:5:2000
 [numberOfFrame,lt,rt,lb,rb ] = courtDetection('1.avi',i);
 [PlayInUpCol,PlayInUpRow,PlayInDownCol,PlayInDownRow] = playerTrack('1.avi',i ,lt,rt,lb,rb)
 pause
